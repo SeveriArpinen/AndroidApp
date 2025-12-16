7ceb26ac903056cca815848ea0a6100cc171a85d
SeveriArpinen <severi.arpinen03@gmail.com>
2025-12-16 12:36:05 +0200

    working proto of todoapp

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -11,10 +11,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,10 +48,12 @@ class MainActivity : ComponentActivity() {
 fun TodoView() {
     val todos = remember {
         mutableStateListOf(
-            Todo("Finish Android project"),
-            Todo("Study Compose")
+            Todo("test"),
+            Todo("AAAAAAAAAAA")
         )
     }
+
+    var newTodo by remember {mutableStateOf("")}
 
     Column (
         modifier = Modifier
@@ -60,7 +67,28 @@ fun TodoView() {
             fontSize = 20.sp,
 
         )
+        TextField(
 
+            value = newTodo,
+            label = {
+                Text("Enter a new TODO")
+            },
+            onValueChange = {newTodo = it}
+        )
+        Button(
+            onClick = {
+                todos.add(
+                    Todo(
+                        newTodo
+                    )
+                )
+                newTodo = ""
+            },
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            Text ("Add a new task")
+        }
         if (todos.isEmpty()){
             Text (
                 text = "No todos yet, add your first!",
@@ -70,10 +98,14 @@ fun TodoView() {
         } else {
             for (todo in todos) {
                 Text(
-                    text = todo.todo
+                    text = todo.todo,
+                    modifier = Modifier
+                        .padding(10.dp)
                 )
             }
         }
 
     }
+
+
 }
